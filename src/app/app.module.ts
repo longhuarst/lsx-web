09ebd5cf44c5724bfe2dcd66a42d441e909d539b
luhuichen <841105197@qq.com>
2020-02-25 16:02:49 +0800

    add ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,15 @@ import { TodomvcComponent } from './todomvc/todomvc.component';
 //引入双向绑定 ngModle
 import {FormsModule} from '@Angular/forms';
 
+// import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { UserloginComponent } from './userlogin/userlogin.component';
+registerLocaleData(zh);
+
 
 @NgModule({
   //组件模块资源：组件/指令/服务
@@ -20,14 +29,24 @@ import {FormsModule} from '@Angular/forms';
     AppComponent,
     HeadComponent,
     ToolbarUserComponent,
-    TodomvcComponent
+    TodomvcComponent,
+    UserloginComponent
   ],
   imports: [//依赖模块
     BrowserModule,
     AppRoutingModule,
     FormsModule,//引入双向绑定 ngModle
+    // HttpClientModule,
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule,
   ],
-  providers: [],
+  /** 配置 ng-zorro-antd 国际化（文案 及 日期） **/
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]//指定启动模块
 })
 export class AppModule { }
+
+
+
